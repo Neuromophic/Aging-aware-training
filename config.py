fbd9ea9e3648c6457f7bcb77caf062f49dedf83a
px3192@kit.edu <px3192@uc2n365.localdomain>
2022-05-23 12:29:47 +0200

    update figures

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ datasets = [k.replace('Dataset_', '').replace('.p', '') for k in files if k.star
 datasets.sort()
 
 # which dataset is selected
-current_dataset = 12
+current_dataset = 5
 
 # learning-rate
 lr = 0.1
